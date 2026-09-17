35bb4fdb36220f3fd599506f28c2c3a525f7cd6e
yogesh7sanap <yogesh7sanap@gmail.com>
2026-09-17 10:06:21 +0530

    python refresher 1 - Assignment - all 12 problems

--- Python_Assignments.py
+++ Python_Assignments.py
@@ -32,19 +32,171 @@ class LeapYear:
                   return False
 
             
-# ----------------------
-# ----------------------
+# Max of 2 numbers ----------------------
 
-# ----------------------
+class Maximum:
+    def __init__(self, n, m):
+        self.n = n
+        self.m = m
+        
+    def solve(self):
+      if self.n>self.m:
+          return self.n
+      else:
+          return self.m
 
-# ----------------------
-# ----------------------
-# ----------------------
 
-# ----------------------
-# ----------------------
-# ----------------------
-# ----------------------
+# Positive to Negative Number ----------------------
+
+class Maximum:
+    def __init__(self, n, m):
+        self.n = n
+        self.m = m
+        
+    def solve(self):
+      if self.n>self.m:
+          return self.n
+      else:
+          return self.m
+
+
+# Multiple of 3 ----------------------
+
+class MultipleOfThree:
+	def __init__(self, n):
+		self.n = n
+
+	def check(self):
+		if self.n % 3 ==0:
+			return True;
+		else:
+			return False
+
+
+# Give a number find Muliple of 5 till that number ----------------------
+
+class MultipleOfFive:
+	def __init__(self, n):
+		self.n = n
+
+	def solve(self):
+          list_multiple_of_five = []
+          for i in range(1,self.n+1):
+               if i%5==0:
+                    list_multiple_of_five.append(i)
+               else:
+                    continue
+          return list_multiple_of_five
+
+
+# Count of positive numbers in a given list ----------------------
+
+class Positive:
+	def __init__(self, arr):
+		self.arr = arr
+
+	def count(self):
+            cnt=0
+            for i in self.arr:
+                  if i>=0:
+                        cnt +=1;
+                  else:
+                        continue
+            return cnt
+
+      
+# Last word length in string ----------------------
+
+class LastWord:
+	def __init__(self, s):
+		self.s = s
+
+	def solve(self):
+            last_word_length = len(self.s.rstrip(' ').split(' ')[-1])
+            return last_word_length
+
+
+# check if integer is palindrome or not ----------------------
+
+
+class Palindrome:
+	def __init__(self, n):
+		self.n = n
+
+	def check(self):
+            if self.n<0:
+                  return False
+            else:
+                  return str(self.n) == str(self.n)[::-1]
+
+# OR
+
+class Palindrome:
+	def __init__(self, n):
+		self.n = n
+
+	def check(self):
+            is_pal=True
+            str_n = str (self.n)
+            for i in range(0, len(str_n)//2):
+                  if str_n[i] != str_n[len(str_n)-1-i]:
+                        is_pal=False
+                  else:
+                        continue
+            return is_pal
+
+      
+# Factorial of a number ----------------------
+
+class Factorial:
+	def __init__(self, n):
+		self.n = n
+
+	def solve(self):
+            factorial=1
+            for i in range(1, self.n+1):
+                  factorial *=i
+            
+            return factorial
+
+
+# Reverse a Integer ----------------------
+
+class Reverse:
+	def __init__(self, n):
+		self.n = n
+
+	def solve(self):
+            reversed_number=0
+            while (self.n>0):
+                  last_digit = self.n %10
+                  reversed_number =reversed_number*10 + last_digit
+                  self.n = self.n //10       
+            return reversed_number
+      
+      
+# Armstrong Number ----------------------
+
+# 153 => len of 153 is 3 hence 1^3 + 5^3 + 3^3 = 153 hence a armstrong number
+
+class Armstrong:
+	def __init__(self, n):
+		self.n = n
+
+	def check(self):
+            num = self.n
+            number_length = len(str(self.n))
+            final_sum =0
+            while self.n >0 :
+                  digit = self.n %10
+                  final_sum += digit ** number_length
+                  self.n = self.n // 10
+            
+            if num == final_sum:
+                  return True
+            else:
+                  return False
+
 
 # ----------------------
 
